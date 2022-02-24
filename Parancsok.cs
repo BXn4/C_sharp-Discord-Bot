651393be2daca90b3e72bf2514d6b2b8dd360956
Bence <78733248+bence9987@users.noreply.github.com>
2022-02-24 21:02:06 +0100

    Add files via upload

---
+++ Parancsok.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Discord.Commands;
+using Discord;
+using Discord.Audio;
+namespace BenCMDSdc
+{
+    public class Parancsok : ModuleBase<SocketCommandContext>
+    {
+        [Command("szia")]
+        public async Task koszones()
+        {
+            var gomb = new ComponentBuilder().WithButton("+", "0", ButtonStyle.Primary);
+            await ReplyAsync($"Helló {Context.Message.Author.Mention}! :wave:", components: gomb.Build());
+        }
+        [Command("play", RunMode= RunMode.Async)]
+        public async Task hang(IVoiceChannel hang = null)
+        {
+            hang = hang ?? (Context.User as IGuildUser)?.VoiceChannel;
+            if (hang == null)
+            {
+                await Context.Channel.SendMessageAsync("Ajjaj! Ahhoz, hogy csatlakozni tudjak a hang csatornához, ahhoz bent kell lenned egy audiócsatornában. Csatlakozz az egyikbe, majd használd újra a parancsot!");
+            }
+
+            var hangkliens = await hang.ConnectAsync();
+        }
+    
+    } 
+
+}
---
+++ Program.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+using Microsoft.Extensions.DependencyInjection;
+using Newtonsoft.Json;
+using System.IO;
+using System.Reflection;
+using Discord.Audio;
+
+namespace dcbot
+{
+    class Program
+    {
+        static void Main(string[] args) => new Program().bot_indit().GetAwaiter().GetResult();
+        private DiscordSocketClient kliens;
+        private CommandService parancsok;
+        private IServiceProvider szolgaltatasok;
+        public string prefix = string.Empty;
+        public string token = string.Empty;
+        public string botneve = string.Empty;
+        public struct config
+        {
+            
+    [JsonProperty("token")]
+            public string Token { get; set; }
+            [JsonProperty("prefix")]
+            public string Prefix { get; set; }
+            [JsonProperty("botneve")]
+            public string Botneve { get; set; }
+        }
+        public async Task bot_indit()
+        {
+            var json = string.Empty;
+            using (var fs = File.OpenRead("config.json"))
+            using (var sr = new StreamReader(fs, new UTF8Encoding(false)))
+            json = await sr.ReadToEndAsync().ConfigureAwait(false);
+            var configjson = JsonConvert.DeserializeObject<config>(json);
+            kliens = new DiscordSocketClient();
+            parancsok = new CommandService();
+            szolgaltatasok = new ServiceCollection()
+                .AddSingleton(kliens)
+                .AddSingleton(parancsok).BuildServiceProvider();
+            var config = new config()
+            {
+                Token = configjson.Token,
+                Prefix = configjson.Prefix,
+                Botneve = configjson.Botneve
+
+            };
+            prefix = config.Prefix;
+            token = config.Token;
+            botneve = config.Botneve;
+            Console.WriteLine($"A {botneve} éppen indul. A parancsok használatához használd a: {prefix} karaktert a parancs elején.");
+            //Console.ReadKey();
+            kliens.Log += kliens_Log;
+            await parancsokregisztral();
+            await kliens.LoginAsync(TokenType.Bot, token);
+            await kliens.StartAsync();
+            await Task.Delay(-1);
+         
+
+        }
+
+        private Task kliens_Log(LogMessage arg)
+        {
+            Console.WriteLine(arg);
+            return Task.CompletedTask;
+        }
+
+        public async Task parancsokregisztral()
+        {
+            kliens.MessageReceived += parancsasyncs;
+            await parancsok.AddModulesAsync(Assembly.GetEntryAssembly(), szolgaltatasok);
+        }
+
+        private async Task parancsasyncs(SocketMessage arg)
+        {
+            var uzenet = arg as SocketUserMessage;
+            var context = new SocketCommandContext(kliens, uzenet);
+            if(uzenet.Author.IsBot)
+            {
+                return;
+            }
+            int argx = 0;
+            if(uzenet.HasStringPrefix(prefix, ref argx))
+            {
+                var eredmeny = await parancsok.ExecuteAsync(context, argx, szolgaltatasok);
+                if (!eredmeny.IsSuccess) Console.WriteLine(eredmeny.ErrorReason);
+            }
+        }
+    }
+}
