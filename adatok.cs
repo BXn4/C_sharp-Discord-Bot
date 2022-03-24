9e89459c8876e02d044b66faed85df2c29de7ae1
Bence <78733248+bence9987@users.noreply.github.com>
2022-03-24 17:19:00 +0100

    Add files via upload

---
+++ adatok.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BenCMDSdc
+{
+    class adatok
+    {
+        public string elso { get; set; }
+        public string kihivott { get; set; }
+        public adatok(string sor)
+        {
+            string[] tomb = sor.Split(',');
+            elso = tomb[0];
+            kihivott = tomb[1];
+        }
+    }
+}
